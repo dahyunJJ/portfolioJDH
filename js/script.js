f0f1dcc3ca4ffc68e79972abc1ac80fd2914fcba
jeongdh <jeongdh1212@gmail.com>
2023-06-15 18:17:49 +0900

    2023.06.15 project 구현

--- js/script.js
+++ js/script.js
@@ -35,3 +35,79 @@ document.addEventListener("scroll", function () {
     hd.style.display = "flex";
   }
 });
+
+// skills 설명 부분 - stack-tab
+const $stackTab = document.querySelector(".stack-tab");
+const $stackIcon = document.querySelectorAll(".icon-list > li");
+
+let stackData;
+fetch("./data/stackData.json")
+  .then((response) => response.json())
+  .then((data) => {
+    stackData = data;
+    stackData.forEach((item, i) => {
+      let HTML = ``;
+
+      const stackItem = item.content
+        .map((content) => `<li>${content}</li>`)
+        .join("");
+
+      HTML += `
+      <div class="stackTextCon">
+                  <h3 class="en">${item.title}</h3>
+                  <ul class="stackText">${stackItem}
+                  </ul>
+                </div>
+      `;
+
+      $stackTab.innerHTML += HTML;
+    });
+
+    // stack tab 기능 구현
+    const $skillText = $stackTab.querySelectorAll("div");
+    $skillText[0].classList.add("on");
+    $stackIcon.forEach((iconitem, index) => {
+      iconitem.addEventListener("click", () => {
+        $stackIcon.forEach((icon, i) => {
+          icon.classList.remove("on");
+          if (index == i) {
+            icon.classList.add("on");
+          }
+        });
+        $skillText.forEach((item, textIndex) => {
+          item.classList.remove("on");
+        });
+        $skillText[index].classList.add("on");
+      });
+    });
+  })
+  .catch((err) => {
+    console.log("error:", err);
+  });
+
+// project 영역 슬라이드
+var swiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let $slides = document.querySelectorAll(".swiper-slide");
+for (let i of $slides) {
+  i.addEventListener("mouseover", function () {
+    swiper.autoplay.stop();
+  });
+  i.addEventListener("mouseout", function () {
+    swiper.autoplay.start();
+  });
+}
